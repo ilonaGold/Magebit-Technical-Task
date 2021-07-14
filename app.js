d04a0b0d7d6f44e1e4523994a7e94bdb149b5189
ilonaGold <songa17@inbox.lv>
2021-07-14 10:41:13 +0300

    Adjusted JS functionality

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
 const agree = document.getElementById("submissionForm-checkbox-input");
 const email = document.getElementById("submissionForm-email-input");
+const arrow = document.getElementById("submissionForm-submission-button-path");
 const subscribe = document.getElementById("submissionForm-submit-button");
-
 const errMsg = document.getElementById("error-message");
-
 const successIcon = document.getElementById("success-icon");
 const thanksMsg = document.getElementById("heading");
 const discountText = document.getElementById("subheading");
 const form = document.getElementById("submissionForm");
 const line = document.getElementById("line-content-divider");
 
-subscribe.addEventListener("click", (e) => {
-    e.preventDefault();
-    checkInputs();
-});
-
 agree.addEventListener("change", (e) => {
     errMsg.textContent = "";
+    checkInputs(email.value.trim().toLowerCase());
+});
+
+email.addEventListener("input", (e) => {
+    checkInputs(email.value.trim().toLowerCase());
 });
 
 function displayError(hintMsg) {
     errMsg.textContent = hintMsg;
     errMsg.style.display = "block";
     email.style.border = "2px solid #E74C3C";
+    subscribe.style.pointerEvents = "none";
+    form.addEventListener("mouseover", function() {
+        arrow.style.fill = "#E3E3E4";
+    })
 }
 
-function checkInputs() {
-
-    const emailValue = email.value.trim();
+function checkInputs(emailValue) {
     const isEmailValid = isEmail(emailValue);
-    
     if (!agree.checked) {
         displayError("You must accept the terms and conditions");
         return
     } 
-
     if (emailValue.length === 0) { 
         displayError("Email address is required");
     } else if (!isEmailValid) { 
@@ -42,17 +41,18 @@ function checkInputs() {
     } else if (emailValue.slice(-2) === "co") { 
         displayError("We are not accepting subscriptions from Colombia emails");
     } else {
-        successIcon.style.display = "block"; 
-        thanksMsg.textContent = "Thanks for subscribing!";
-        thanksMsg.style.fontSize = "21px";
-        discountText.textContent = "You have successfully subscribed to our email listing. Check your email for the discount code.";
-        form.style.display = "none";
-        line.style.boxShadow = "none";
+        subscribe.style.pointerEvents = "auto";
+        errMsg.style.display = "none";
+        email.style.border = "1px solid #E3E3E4";
+        email.style.borderLeft = "4px solid #4066A5";
+        form.addEventListener("mouseover", function() {
+            arrow.style.fill = "#4066A5";
+        })
     }
 }
 
 function isEmail(email) {
-    
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    return /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/.test(email);
 }
+
 
